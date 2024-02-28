eb3b7c3e79cc7908a01f6bbc6d56b632415eadf4
EsaiXD <esaixd2@gmail.com>
2024-02-28 09:21:14 +0000

    cambio de nombre

---
+++ alumno.py
@@ -0,0 +1,14 @@
+class alumno:
+    
+    
+    def imprimir(self):
+        self.nota = 0
+        self.nombre = ""
+        print(self.nombre, "obtiene" ,self.nota)
+        
+        
+    def promociona(self):
+        self.nota = 0
+        self.nombre = ""
+        if (self.nota >= 5):
+            print("promociona")
