f84e3bc1f97d0b2e5bd0a3cc66f5de73cd50a72d
EsaiXD <esaixd2@gmail.com>
2024-02-28 09:58:56 +0000

    seguimos después

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from alumno import alumno
 def main():
     daniel = alumno("Daniel", 5)
     daniel.imprimir()
+    daniel.promociona()
     
     
 if __name__ == "__main__":
---
+++ persona.py
@@ -0,0 +1,10 @@
+class persona:
+    def __init__(self, nombre, anos):
+        self.anos = anos 
+        self.nombre = nombre
+        
+        
+def imprimir(self):
+        self.anos = 0
+        self.nombre = ""
+        print(self.nombre, "tiene" ,self.)
