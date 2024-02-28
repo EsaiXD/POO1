6fa76388e89e5dea2114fd25bde1a5c36fcaecc9
EsaiXD <esaixd2@gmail.com>
2024-02-28 12:42:39 +0000

    creado Obama

--- persona.py
+++ persona.py
@@ -1,10 +1,22 @@
-class persona:
+class Persona:
     def __init__(self, nombre, anos):
         self.anos = anos 
         self.nombre = nombre
         
         
-def imprimir(self):
-        self.anos = 0
-        self.nombre = ""
-        print(self.nombre, "tiene" ,self.)
+    def imprimir(self):
+        print(self.nombre, "tiene" ,self.anos, "años")
+        
+        
+        
+        
+    def cumpleanos(self):
+        self.anos += 1
+        self.imprimir()
+        
+        
+if __name__ == "__main__":
+    obama = Persona("Obama", 62)
+    obama.imprimir()
+    obama.cumpleanos()
+    
